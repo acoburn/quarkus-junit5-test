5e6caeb240314c18fe5661fd1657ecc4e4fdc732
Aaron Coburn <acoburn@apache.org>
2019-10-05 12:24:43 -0400

    Add nested JUnit5 class

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,6 +1,7 @@
 package org.acme.quickstart;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,13 +10,15 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+    @Nested
+    public class NestedClass {
+        @Test
+        public void testHelloEndpoint() {
+            given()
+              .when().get("/hello")
+              .then()
+                 .statusCode(200)
+                 .body(is("hello"));
+        }
     }
-
 }
